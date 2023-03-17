a80f886e5453060744ce105d85eafbc5430272d3
Marvin Buss <marvin.buss@gmail.com>
2023-03-17 18:45:38 +0100

    Add custom landing zone config

--- code/locals.tf
+++ code/locals.tf
@@ -3,4 +3,13 @@ locals {
   root_name        = var.root_name
   default_location = var.default_location
   default_tags     = var.default_tags
+
+  custom_landing_zones = {
+    "${local.root_id}-cloud-native" = {
+      display_name               = "Cloud Native"
+      parent_management_group_id = "${local.root_id}-landing-zones"
+      subscription_ids           = local.mg_cloud_native_subscription_id_overrides
+      archetype_config           = local.mg_cloud_native_archetype_config_overrides
+    }
+  }
 }
