ad39b116ef16472ba3a161bf2d180e9a65c287ce
Marvin Buss <marvin.buss@gmail.com>
2023-03-20 19:54:48 +0100

    lint

--- code/management.tf
+++ code/management.tf
@@ -2,7 +2,7 @@ module "management" {
   source = "./modules/management"
   providers = {
     azurerm = azurerm.management
-   }
+  }
 
   location = local.management_location
   root_id  = local.root_id
