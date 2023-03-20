acade9ef7894ac5549fcf0ff4cd6ed97d89620aa
Marvin Buss <marvin.buss@gmail.com>
2023-03-20 19:51:40 +0100

    Add provider and configuration

--- code/main.tf
+++ code/main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/time"
       version = "0.9.1"
     }
+    azapi = {
+      source  = "azure/azapi"
+      version = "1.4.0"
+    }
   }
 
   backend "azurerm" {
@@ -41,4 +45,38 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = true
     }
   }
+}
+
+provider "azurerm" {
+  alias                          = "management"
+  subscription_id                = local.management_subscription_id
+  disable_correlation_request_id = false
+  environment                    = "public"
+  skip_provider_registration     = false
+  storage_use_azuread            = true
+  use_oidc                       = true
+
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults   = true
+      recover_soft_deleted_certificates = true
+      recover_soft_deleted_keys         = true
+      recover_soft_deleted_secrets      = true
+    }
+    network {
+      relaxed_locking = true
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  }
+}
+
+provider "azapi" {
+  default_location               = local.default_location
+  default_tags                   = local.default_tags
+  disable_correlation_request_id = false
+  environment                    = "public"
+  skip_provider_registration     = false
+  use_oidc                       = true
 }
